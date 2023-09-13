fbc950fc1ed45119db3b8fc9d806be88a63f5cc7
DDU <100361038+Andrea-Puddu@users.noreply.github.com>
2023-09-13 16:57:45 +0200

    added ChannelDetail, SearchFeed and VideoDeail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -22,9 +22,9 @@ const VideoCard = ({
     <Card sx={{width: {xs: '100%', sm: '358px', md: '320px'}, boxShadow: 'none', borderRadius: 0}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{width: {xs: '100%', sm: '358px'}, height: 180}}
+          sx={{width: {xs: '100%', sm: '358px', md: '320px'}, height: 180}}
         />
       </Link>
       <CardContent sx={{backgroundColor: '#1e1e1e', height: '106px'}}>
